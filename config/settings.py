61deafe568d5f95df3f2d1614e6d8a1b04ebd4ff
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-05-30 22:10:00 +0300

    feat: Add support for static and media files [#7]

--- config/settings.py
+++ config/settings.py
@@ -85,12 +85,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = 'static_root'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media_root'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
