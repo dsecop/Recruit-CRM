1cc2750bba2018179c7b0174bbf4c177e6ca7727
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-01 22:52:31 +0300

    feat: Create applicant model [#8]

--- applicants/models.py
+++ applicants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from tags.models import Tag
+
+
+class Applicant(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    title = models.CharField(max_length=30, blank=True)
+    note = models.TextField(max_length=200, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(unique=True, max_length=120, verbose_name='email address')
+    tags = models.ManyToManyField(Tag, blank=True)
+    profile_picture = models.ImageField(blank=True, null=True, upload_to='profile_pictures/')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
