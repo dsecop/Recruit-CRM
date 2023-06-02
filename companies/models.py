27a28a00d2dff6d95ba587f8cbbe88154199cc5a
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-02 22:52:48 +0300

    feat: Create company model [#2]

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    website = models.CharField(max_length=200, blank=True, null=True)
+    owner = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'companies'
+
+    def __str__(self):
+        return f'{self.name}'
