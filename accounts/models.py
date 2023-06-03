d98cb2de9830dd5f2570207226ab0b8f0240232e
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-03 20:50:35 +0300

    fix: Change field name [#25]

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    is_company = models.BooleanField(default=True)
+    is_company_owner = models.BooleanField(default=True)
     is_recruiter = models.BooleanField(default=False)
     email = models.EmailField(unique=True, max_length=120, verbose_name='email address')
