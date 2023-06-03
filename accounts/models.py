f7c44107d2ef24319f66f4163049744316a7085b
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-03 20:41:58 +0300

    fix: Change field name and default value [#25]

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    is_applicant = models.BooleanField(default=False)
-    is_recruiter = models.BooleanField(default=True)
+    is_company = models.BooleanField(default=True)
+    is_recruiter = models.BooleanField(default=False)
     email = models.EmailField(unique=True, max_length=120, verbose_name='email address')
