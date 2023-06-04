d518c61b37b88e5751a9a0753c4d4f5eef1e64be
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-04 21:29:39 +0300

    feat: Add company field to the applicant model [#8]

--- applicants/models.py
+++ applicants/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from accounts.models import CustomUser
 from tags.models import Tag
+from companies.models import Company
 
 
 class Applicant(models.Model):
@@ -14,6 +15,7 @@ class Applicant(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     profile_picture = models.ImageField(blank=True, null=True, upload_to='profile_pictures/')
     recruiter = models.ForeignKey(CustomUser, related_name='applicants', null=True, blank=True, on_delete=models.SET_NULL)
+    company = models.ForeignKey(Company, related_name='applicants', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
