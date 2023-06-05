96ad5992fa5e62d3bfcecdec50aed175f97d2b7c
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-05 21:22:29 +0300

    feat: Add applicant create view to the url dispatcher [#15]

--- applicants/urls.py
+++ applicants/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from applicants.views import ApplicantListView
+from applicants.views import ApplicantListView, ApplicantCreateView
 
 app_name = 'applicants'
 
 urlpatterns = [
     path('', ApplicantListView.as_view(), name='applicant-list'),
+    path('create/', ApplicantCreateView.as_view(), name='applicant-create'),
 ]
