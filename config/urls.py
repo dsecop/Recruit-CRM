01409f3fb2df12b5b28ad6ed4048818fe3261cb1
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-10 21:14:12 +0300

    feat: Add accounts app url to the main url dispatcher [#4]

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('applicants/', include('applicants.urls', namespace='applicants')),
     path('companies/', include('companies.urls', namespace='companies')),
 ]
