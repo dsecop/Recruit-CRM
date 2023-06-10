fa2a8bd1ef759ea38158c7283efd9fee18dc5d91
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-10 21:07:39 +0300

    feat: Add login, logout and signup views to the url dispatcher [#4]

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
+from accounts.views import SignUpView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path(),
+    path('login/', auth_view.LoginView.as_view(), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
