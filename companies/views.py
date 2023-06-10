145e4242f7a72ccc4b87784ac66977aa70000a42
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-10 17:20:57 +0300

    feat: Add company create view [#11]

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from companies.forms import CompanyCreateForm
+from companies.models import Company
+
+
+class CompanyCreateView(generic.CreateView):
+    form_class = CompanyCreateForm
+    template_name = 'company_create.html'
+    success_url = reverse_lazy('applicants:applicant-list')
+
+    def dispatch(self, request, *args, **kwargs):
+        if Company.objects.filter(owner=self.request.user):
+            return redirect('applicants:applicant-list')
+        return super().dispatch(request, *args, **kwargs)
+
+    @transaction.atomic
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
