163fd40a749e0145d92a041b8aec45a07fc1f139
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-11 21:53:47 +0300

    feat: Create ApplicantUpdateForm form [#16]

--- applicants/forms.py
+++ applicants/forms.py
@@ -6,3 +6,16 @@ class ApplicantCreateForm(forms.ModelForm):
     class Meta:
         model = Applicant
         fields = ('first_name', 'last_name', 'email')
+
+
+class ApplicantUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Applicant
+        fields = ('first_name',
+                  'last_name',
+                  'email',
+                  'title',
+                  'city',
+                  'profile_picture',
+                  'note',
+                  )
