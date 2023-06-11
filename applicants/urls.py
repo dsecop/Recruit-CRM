418b9e9257d496cb662cba448228b74235fe4fed
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-11 16:00:08 +0300

    feat: Add ApplicantDetailView to the url dispatcher [#14]

--- applicants/urls.py
+++ applicants/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from applicants.views import ApplicantListView, ApplicantCreateView
+from applicants.views import ApplicantListView, ApplicantCreateView, ApplicantDetailView
 
 app_name = 'applicants'
 
 urlpatterns = [
     path('', ApplicantListView.as_view(), name='applicant-list'),
     path('create/', ApplicantCreateView.as_view(), name='applicant-create'),
+    path('<int:pk>/', ApplicantDetailView.as_view(), name='applicant-detail'),
 ]
