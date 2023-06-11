8f6589ccfe3d891c5b1cdfcf9ff4150c3a7cedf1
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-11 13:37:21 +0300

    feat: Add HomePageView to the main url dispatcher [#9]

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePageView.as_view(), name='home-page'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('applicants/', include('applicants.urls', namespace='applicants')),
     path('companies/', include('companies.urls', namespace='companies')),
