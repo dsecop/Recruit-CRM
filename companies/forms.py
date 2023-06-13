816a2d5601eea0aa044a7b195d9de572b834c233
Dariusz Klimowicz <dariusz.secop@gmail.com>
2023-06-13 22:17:22 +0300

    feat: Create a form for creating a new recruiter [#21]

--- companies/forms.py
+++ companies/forms.py
@@ -1,8 +1,18 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
 from companies.models import Company
+
+User = get_user_model()
 
 
 class CompanyCreateForm(forms.ModelForm):
     class Meta:
         model = Company
         fields = ('name',)
+
+
+class RecruiterCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
